00d3c39f471cfb71b3567f1cf3b84dcad7b0e9ba
rottenberry23 <raohq8@gmail.com>
2024-05-25 22:10:14 +0500

    Added radio button functionality to the donor page

--- BePlus/obj/Debug/net6.0/BePlus.AssemblyInfo.cs
+++ BePlus/obj/Debug/net6.0/BePlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92fcddc4bfa12fd2585442b3699cc72278fcc7f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
