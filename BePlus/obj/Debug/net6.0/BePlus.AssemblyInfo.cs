91ad6a44ad39fbb3a46bb9e30a237d2c9c8d822c
rottenberry23 <raohq8@gmail.com>
2024-05-26 20:17:34 +0500

    Template is ready, waiting for DB stuff to make it fully functional

--- BePlus/obj/Debug/net6.0/BePlus.AssemblyInfo.cs
+++ BePlus/obj/Debug/net6.0/BePlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba8364feceeca44e3332bf10137761fa75d0ef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20b51af42137e3ebb06d5604f7e6eb69c3bf6cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BePlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
